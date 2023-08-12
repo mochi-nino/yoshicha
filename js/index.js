609dd9ff4bc9128ab3294f1493ad20a6b3337256
mochi_nino <z898911z@gmail.com>
2023-08-12 19:50:13 +0800

    更新bug

--- js/index.js
+++ js/index.js
@@ -82,8 +82,27 @@ slides.addEventListener('touchend', function () {
 // });
 
 $(function () {
-    $(window).on('load scroll', function () {
+    $(window).ready(function () {
         $('.main').each(function () {
+            //ターゲットの位置を取得
+            var target = $(this).offset().top
+            //スクロール量を取得
+            var scroll = $(window).scrollTop()
+            //ウィンドウの高さを取得
+            var height = $(window).height()
+            //ターゲットまでスクロールするとフェードインする
+            if (scroll > target - height) {
+                //クラスを付与
+                $(this).addClass('active')
+            }
+        });
+    });
+});
+
+
+$(function () {
+    $(window).ready(function () {
+        $('.news').each(function () {
             //ターゲットの位置を取得
             var target = $(this).offset().top
             //スクロール量を取得
@@ -116,26 +135,6 @@ $(function () {
         });
     });
 });
-
-
-$(function () {
-    $(window).on('load scroll', function () {
-        $('.news').each(function () {
-            //ターゲットの位置を取得
-            var target = $(this).offset().top
-            //スクロール量を取得
-            var scroll = $(window).scrollTop()
-            //ウィンドウの高さを取得
-            var height = $(window).height()
-            //ターゲットまでスクロールするとフェードインする
-            if (scroll > target - height) {
-                //クラスを付与
-                $(this).addClass('active')
-            }
-        });
-    });
-});
-
 
 $(function () {
     $(window).on('load scroll', function () {
